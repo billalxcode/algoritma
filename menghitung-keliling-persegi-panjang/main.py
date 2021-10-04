d5ff7160dfbe71d680fbb384d0539808f7208f67
404rgr <pausi.git@gmail.com>
2021-10-04 16:15:31 -0400

    Menambahkan: menghitung keliling persegi panjang

---
+++ menghitung-keliling-persegi-panjang/main.py
@@ -0,0 +1,12 @@
+"""
+    Created by pausi
+    04-10-20021
+"""
+
+# Rumus:
+# Keliling = 2 x (Panjang + Lebar)
+
+panjang = int(input("Panjang: "))
+lebar   = int(input("Lebar: "))
+hasil   = 2*(panjang+lebar)
+print("Keliling: " + str(hasil))
